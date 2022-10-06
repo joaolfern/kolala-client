66e914b4a9d4c3903ef406cac1ae3614d9a5f26e
João Luiz <joaoaoluiz@protonmail.com>
2022-10-05 23:12:54 -0300

    Add items to category resource

--- screens/EventForm/constants.tsx
+++ screens/EventForm/constants.tsx
@@ -6,7 +6,32 @@ import announcement from '../../assets/mapIcons/announcement.png'
 import { ImageURISource } from 'react-native'
 import { asResource } from './utils'
 
-export const CATEGORY_RESOURCE: Item[] = [{ label: 'Música', value: 0 }]
+export const CATEGORY_RESOURCE: Item[] = [
+  {
+    label: 'Cultura',
+    value: 4,
+  },
+  {
+    label: 'Comida',
+    value: 2,
+  },
+  {
+    label: 'Música',
+    value: 1,
+  },
+  {
+    label: 'Esporte',
+    value: 3,
+  },
+  {
+    label: 'Tecnologia',
+    value: 0,
+  },
+  {
+    label: 'Educação',
+    value: 0,
+  },
+]
 
 export const MAP_ICONS: { [key: number]: ImageURISource } = {
   0: coffee,
