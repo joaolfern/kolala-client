250024221f2f8ab2540053ca7b71030cc9e5d44c
João Luiz <joaoaoluiz@protonmail.com>
2022-11-05 17:50:17 -0300

    Add message list

--- Models/Message.ts
+++ Models/Message.ts
@@ -1,4 +1,19 @@
+import { AxiosRequestConfig } from 'axios'
+import api from '../services/api'
+import { Fetch } from '../services/Fetch'
 import { IProfile } from '../types/Profile'
+
+class Message {
+  private path = 'chat'
+
+  async list (config: AxiosRequestConfig & { params: any }) {
+    return (
+      await Fetch<IMessage[]>(
+        () => api.get(this.path, config)
+      )
+    )
+  }
+}
 
 export interface IMessage {
   id: number
@@ -16,3 +31,5 @@ export interface ISendMessageRequest {
   content: string
   answerToId?: number
 }
+
+export default new Message()
