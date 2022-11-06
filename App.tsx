2311684dc8cce5051fe42a72aabee059a2a9caac
João Luiz <joaoaoluiz@protonmail.com>
2022-11-06 16:42:43 -0300

    Move dayjs extends to App

--- App.tsx
+++ App.tsx
@@ -12,10 +12,25 @@ import Toast from './components/Toast/Toast'
 import { toastRef } from './utils/toast'
 import { ActionSheetProvider } from '@expo/react-native-action-sheet'
 
+import relativeTime from 'dayjs/plugin/relativeTime'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import localeData from 'dayjs/plugin/localeData'
+import dayjsBR from 'dayjs/locale/pt-br'
+import dayjs from 'dayjs'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(localeData)
+dayjs.extend(relativeTime)
+dayjs.locale(dayjsBR)
+
 LogBox.ignoreLogs(['EventEmitter.removeListener'])
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
+
+  console.log(dayjs().toDate())
 
   if (!isLoadingComplete) {
     return null
