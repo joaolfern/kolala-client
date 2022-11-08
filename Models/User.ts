33f5ad6aecd52908ed335d6b67f94df54e0d717b
João Luiz <joaoaoluiz@protonmail.com>
2022-11-07 23:10:59 -0300

    Add visually picture select to profile form

--- Models/User.ts
+++ Models/User.ts
@@ -11,17 +11,35 @@ export type IUserPromoteConfig = {
   }
 }
 
+export type IUserUpdateProfileConfig = {
+  body: Partial<IProfile>
+  id: number
+}
+
 class User {
-  getProfile = async (id: number) => (
-    await Fetch<IProfileViewData>(
+  async getProfile (id: number) {
+    return await Fetch<IProfileViewData>(
       () => api.get(`auth/users/profile/${id}`)
     )
-  )
-  promote = async ({ targetId, body }: IUserPromoteConfig) => (
-    await Fetch<string>(
+  }
+
+  async updateProfile ({ body, id }: IUserUpdateProfileConfig) {
+    return await Fetch<string>(
+      () => api.patch(`auth/users/profile/${id}`, body, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      })
+    )
+  }
+
+  async promote ({ targetId, body }: IUserPromoteConfig)  {
+    return await Fetch<string>(
       () => api.post(`auth/users/promote/${targetId}`, body)
     )
-  )
+  }
+
+
 }
 
 export default new User()
--- screens/ProfileForm/ProfileForm.tsx
+++ screens/ProfileForm/ProfileForm.tsx
@@ -1,17 +1,38 @@
+import { MaterialIcons } from '@expo/vector-icons'
+import { useNavigationState } from '@react-navigation/native'
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { useForm } from 'react-hook-form'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import Avatar from '../../components/Avatar/Avatar'
 import Header from '../../components/Header/Header'
 import SafeAreaView from '../../components/SafeAreaView/SafeAreaView'
 import Scroll from '../../components/Scroll/Scroll'
+import Span from '../../components/Span/Span'
 import Colors from '../../constants/Colors'
+import { IUserUpdateProfileConfig } from '../../Models/User'
+import { RootStackParamList } from '../../types'
+import PictureButton from './components/PictureButton'
+
+export type ProfileFormEvent = IUserUpdateProfileConfig['body'] & {}
 
 function ProfileForm() {
+  const { profile } = useNavigationState(
+    state =>
+      state.routes.find(item => item.name === 'ProfileForm')
+        ?.params as RootStackParamList['ProfileForm']
+  )
+
+  const { control } = useForm<ProfileFormEvent>()
+
   return (
     <Scroll style={styles.Container}>
       <SafeAreaView>
         <Header>
           <Header.Title>Editar perfil</Header.Title>
         </Header>
+        <Span style={styles.Row}>
+          <PictureButton name='picture' profile={profile} />
+        </Span>
       </SafeAreaView>
     </Scroll>
   )
@@ -24,5 +45,13 @@ const styles = StyleSheet.create({
     height: '100%',
     flex: 1,
     backgroundColor: Colors.background,
+  },
+  Row: {
+    position: 'relative',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 18,
   },
 })
