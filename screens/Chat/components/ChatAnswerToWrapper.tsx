555b0a166d10eb1f4c054288256285166ea2b2d2
João Luiz <joaoaoluiz@protonmail.com>
2022-11-07 21:08:14 -0300

    Fix chat reply background color

--- screens/Chat/components/ChatAnswerToWrapper.tsx
+++ screens/Chat/components/ChatAnswerToWrapper.tsx
@@ -3,18 +3,23 @@ import { IMessage } from '../../../Models/Message'
 import Span from '../../../components/Span/Span'
 import { StyleSheet } from 'react-native'
 import ReplyPreview from '../../../components/ReplyPreview/ReplyPreview'
+import Colors from '../../../constants/Colors'
 
 interface IProps {
   message: IMessage
   children: ReactNode
+  isAuthor: boolean
 }
 
-function ChatAnswerToWrapper({ children, message }: IProps) {
+function ChatAnswerToWrapper({ children, message, isAuthor }: IProps) {
   if (message.answerToId)
     return (
       <Span style={styles.Container}>
         <ReplyPreview
-          style={styles.ReplyPreview}
+          style={[
+            styles.ReplyPreview,
+            ...(isAuthor ? [] : [styles.ReplyPreviewNotAuthor]),
+          ]}
           title={message.answerTo?.author?.name}
           content={message.answerTo?.content}
         />
@@ -33,5 +38,8 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
     paddingVertical: 7,
+  },
+  ReplyPreviewNotAuthor: {
+    backgroundColor: Colors.chatTextbox,
   },
 })
