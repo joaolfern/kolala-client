04716cafeba768431b5074ce10affe10dc75987f
João Luiz <joaoaoluiz@protonmail.com>
2022-11-30 16:35:56 -0300

    Fix android build

---
+++ env.ts
@@ -0,0 +1,7 @@
+export const REACT_APP_SERVER = 'https://koalala.loca.lt'
+export const ENVIRONMENT = 'local'
+export const ANDROID_CLIENT_ID = '925600786502-85n72luaobffp5kakds734avc8bfa6c4.apps.googleusercontent.com'
+export const EXPO_CLIENT_ID = '925600786502-035m1v9jcrjd0o4j6ld3l5iof8br8id8.apps.googleusercontent.com'
+export const GOOGLE_API_TOKEN = 'AIzaSyCPoBRfc1HtIhq0czj3jdhNgCC4b3eaccU'
+
+export const EXPO_FB_CLIENT_ID = '441695687797065'
