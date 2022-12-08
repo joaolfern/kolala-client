9d556d36a004a4a8e54e120dc4c87059edca0fb1
João Luiz <joaoaoluiz@protonmail.com>
2022-12-08 15:40:06 -0300

    Fix server address

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
-export const REACT_APP_SERVER = 'https://koalala.loca.lt'
-// 'https://koalala.loca.lt'
+export const REACT_APP_SERVER = 'https://kolala.loca.lt'
+// 'https://kolala.loca.lt'
 export const ENVIRONMENT = 'production'
 export const ANDROID_CLIENT_ID = '925600786502-85n72luaobffp5kakds734avc8bfa6c4.apps.googleusercontent.com'
 export const EXPO_CLIENT_ID = '925600786502-035m1v9jcrjd0o4j6ld3l5iof8br8id8.apps.googleusercontent.com'
